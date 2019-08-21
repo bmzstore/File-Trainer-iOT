5e1970b010d77a286891df1533e834e7c4ce845a
Dicky B_Mz <50789462+bmzstore@users.noreply.github.com>
2019-08-22 06:33:33 +0700

    Add files via upload

---
+++ Project_2_Mengukur_Jarak_menggunakan_LCD_OLED.ino
@@ -0,0 +1,46 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define TRIGGER_PIN  5
+#define ECHO_PIN     4
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(BUILTIN_LED, OUTPUT);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(TRIGGER_PIN, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10); // Added this line
+  digitalWrite(TRIGGER_PIN, LOW);
+  duration = pulseIn(ECHO_PIN, HIGH);
+  distance = (duration/2) / 29.1;
+  Serial.print(distance);
+  Serial.println(" cm");
+  display.setTextSize(1);
+  display.setCursor(0,15);
+  display.print("Jarak: ");
+  display.setTextSize(2);
+  display.setCursor(0,25);
+  display.print(distance);
+  display.setTextSize(2);
+  display.print(" Cm");
+  display.display(); 
+  delay(1000);
+}
