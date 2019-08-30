4367103f87e8e3e0bbfd83692e3efead17bf26d8
Dicky B_Mz <50789462+bmzstore@users.noreply.github.com>
2019-08-31 06:42:11 +0700

    Update Project_11_Interface_Nodemcu_dengan_LCD_OLED.ino

--- Project_11_Interface_Nodemcu_dengan_LCD_OLED.ino
+++ Project_11_Interface_Nodemcu_dengan_LCD_OLED.ino
@@ -2,7 +2,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-
+//Koneksi LCD-> SCL Ke D1 dan SDA ke D2
 #define OLED_RESET D5
 /* Object named display, of the class Adafruit_SSD1306 */
 Adafruit_SSD1306 display(OLED_RESET);
